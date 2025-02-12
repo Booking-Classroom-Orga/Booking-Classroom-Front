392d7d9d903bd929d1b3230217a65c33cd85653c
CassandraDM <cassandra.delmas-marchiset@mail-esd.com>
2025-02-12 11:56:16 +0100

    edit pt.1

--- src/components/custom-ui/edit-dialog.tsx
+++ src/components/custom-ui/edit-dialog.tsx
@@ -7,48 +7,130 @@ import {
   DialogHeader,
   DialogFooter,
 } from "../ui/dialog";
-import { Label } from "../ui/label";
 import { Button } from "../ui/button";
 import { Input } from "../ui/input";
+import { Form, FormField, FormItem, FormLabel, FormControl } from "../ui/form";
+import { Textarea } from "../ui/textarea";
+import { update, findById } from "@/services/classroom.service";
+import { useForm } from "react-hook-form";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 
 const UpdateDialog = () => {
+  const { id } = useParams();
+  const form = useForm();
+  const [open, setOpen] = useState(false);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      if (id) {
+        const classroom = await findById(Number(id));
+        form.reset({
+          name: classroom.name,
+          capacity: classroom.capacity,
+          equipement: classroom.equipement.join(", "),
+        });
+      }
+    };
+
+    if (open) {
+      fetchData();
+    }
+  }, [id, open, form]);
+
+  const onSubmit = async (data: any) => {
+    const parsedData = {
+      ...data,
+      equipement: data.equipement
+        ? data.equipement.split(",").map((item: string) => item.trim())
+        : [],
+    };
+
+    await update(Number(id), parsedData);
+    setOpen(false);
+  };
+
   return (
-    <Dialog>
+    <Dialog open={open} onOpenChange={setOpen}>
       <DialogTrigger asChild>
-        <Button variant="outline">Edit</Button>
+        <Button variant="outline" onClick={() => setOpen(true)}>
+          Edit
+        </Button>
       </DialogTrigger>
       <DialogContent className="sm:max-w-[425px]">
         <DialogHeader>
-          <DialogTitle>Edit profile</DialogTitle>
+          <DialogTitle>Edit Classroom</DialogTitle>
           <DialogDescription>
             Make changes to your profile here. Click save when you're done.
           </DialogDescription>
         </DialogHeader>
-        <div className="grid gap-4 py-4">
-          <div className="grid grid-cols-4 items-center gap-4">
-            <Label htmlFor="name" className="text-right">
-              Name
-            </Label>
-            <Input
-              id="name"
-              defaultValue="Pedro Duarte"
-              className="col-span-3"
-            />
-          </div>
-          <div className="grid grid-cols-4 items-center gap-4">
-            <Label htmlFor="username" className="text-right">
-              Username
-            </Label>
-            <Input
-              id="username"
-              defaultValue="@peduarte"
-              className="col-span-3"
-            />
-          </div>
-        </div>
-        <DialogFooter>
-          <Button type="submit">Save changes</Button>
-        </DialogFooter>
+        <Form {...form}>
+          <form className="grid gap-4" onSubmit={form.handleSubmit(onSubmit)}>
+            <div className="grid gap-4 py-4">
+              <FormField
+                control={form.control}
+                name="name"
+                render={({ field }) => (
+                  <FormItem className="grid grid-cols-4 items-center gap-4">
+                    <FormLabel htmlFor="name" className="text-right">
+                      Name
+                    </FormLabel>
+                    <FormControl>
+                      <Input
+                        id="name"
+                        placeholder="eg. Class A"
+                        className="col-span-3"
+                        {...field}
+                      />
+                    </FormControl>
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="capacity"
+                render={({ field }) => (
+                  <FormItem className="grid grid-cols-4 items-center gap-4">
+                    <FormLabel htmlFor="capacity" className="text-right">
+                      Capacity
+                    </FormLabel>
+                    <FormControl>
+                      <Input
+                        id="capacity"
+                        placeholder="eg. 30"
+                        className="col-span-3"
+                        type="number"
+                        {...field}
+                      />
+                    </FormControl>
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="equipement"
+                render={({ field }) => (
+                  <FormItem className="grid grid-cols-4 items-center gap-4">
+                    <FormLabel htmlFor="equipement" className="text-right">
+                      Equipement(s)
+                    </FormLabel>
+                    <FormControl>
+                      <Textarea
+                        id="equipement"
+                        placeholder="eg. Projector, Whiteboard"
+                        className="col-span-3"
+                        {...field}
+                      />
+                    </FormControl>
+                  </FormItem>
+                )}
+              />
+            </div>
+            <DialogFooter>
+              <Button type="submit">Save</Button>
+            </DialogFooter>
+          </form>
+        </Form>
       </DialogContent>
     </Dialog>
   );
